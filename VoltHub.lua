b78db102f49ba4204ded8c651d87bef13c54b13b
Dhon <dhon20640@gmail.com>
2026-06-08 04:52:32 -0300

    Refactor game loading mechanism with GameId support

--- VoltHub.lua
+++ VoltHub.lua
@@ -1,48 +1,15 @@
--- // VoltHub Game Selector
-
-local CurrentPlaceId = game.PlaceId
-
-local GameList = {
-
-    ["Blox Fruits"] = {
-        PlaceIds = {
-            2753915549,   85211729168715,  -- Sea 1
-            4442272183,   79091703265657,  -- Sea 2
-            7449423635,   100117331123089, -- Sea 3
-        },
-        Script = "https://raw.githubusercontent.com/Dev-VoltHub/Bloxfruit/refs/heads/main/volt.lua"
-    },
-
-    ["Steal a Brainrot"] = {
-        PlaceIds = {
-            109983668079237,
-            96342491571673,
-        },
-        Script = "https://raw.githubusercontent.com/Dev-VoltHub/StealBrainrot/refs/heads/main/Volt.lua"
-    },
-
-    ["99 Nights in the Florest"] = {
-        PlaceIds = {
-            79546208627805,
-        },
-        Script = "https://raw.githubusercontent.com/Dev-VoltHub/99NightsFlorest/refs/heads/main/volt.lua"
-    },
-
+local CurrentGameId = game.GameId
+local SupportedGames = {
+    [994732206]  = "https://raw.githubusercontent.com/Dev-VoltHub/Bloxfruit/refs/heads/main/volt.lua",             -- Blox Fruits
+    [7709344486] = "https://raw.githubusercontent.com/Dev-VoltHub/StealBrainrot/refs/heads/main/Volt.lua",         -- Steal a Brainrot
+    [7326934954] = "https://raw.githubusercontent.com/Dev-VoltHub/99NightsFlorest/refs/heads/main/volt.lua",       -- 99 Nights in the Forest
 }
 
--- // Loader
-
-local function LoadGame(GameData)
-    loadstring(game:HttpGet(GameData.Script))()
-end
-
-for GameName, GameData in pairs(GameList) do
-    for _, PlaceId in pairs(GameData.PlaceIds) do
-        if CurrentPlaceId == PlaceId then
-            LoadGame(GameData)
-            return
-        end
+for GameId, Script in pairs(SupportedGames) do
+    if CurrentGameId == GameId then
+        loadstring(game:HttpGet(Script))()
+        return
     end
 end
 
-game:GetService("Players").LocalPlayer:Kick("Game Not Supported [❌]")
+game:GetService("Players").LocalPlayer:Kick("Unsupport Game")
